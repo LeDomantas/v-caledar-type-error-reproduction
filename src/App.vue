cf14026f9121abb957b013181081772cf4c5e2b5
Domantas Lekavicius <domantas.lekavicius@gmail.com>
2022-05-12 18:03:27 +0300

    minimal  error reproduction

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { DatePicker } from "v-calendar";
+import { ref } from "vue";
+
+const date = ref();
+</script>
+
+<template>
+  <DatePicker v-model="date" :columns="$screens({ xs: 1, sm: 2 })">
+    <template v-slot="{ inputValue, inputEvents }">
+      <input
+        class="bg-white border px-2 py-1 rounded"
+        :value="inputValue"
+        v-on="inputEvents"
+      />
+    </template>
+  </DatePicker>
+</template>
